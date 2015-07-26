233b89933a747d279b2cf4d4d6ff5604e0d58026
Sig Lange <sig.lange@gmail.com>
2015-07-26 14:46:32 -0700

    updated css

--- asset/picture.html.gen.go
+++ asset/picture.html.gen.go
@@ -3,5 +3,5 @@
 package asset
 
 var picture = Blob(asset.init(asset{Name: "picture.html", Content: "" +
-	"<html>\n<head>\n   <title>Pictures in {{$.Path}}</title>\n   <link href=\"/s/style.css\" media=\"all\" rel=\"stylesheet\" />\n</head>\n<body>\n\n{{range $i, $b := .Breadcrum}}\n<a href=\"{{$b.Url}}\">{{$b.Name}}</a> \n{{end}}\n<br>\n\n<h2>{{.Title}} - {{$.LocalPath}}</h2>\n\n{{range $i, $f := .Directories}}\n<a href=\"{{$f.Url}}\">{{$f.Basename}}</a> &nbsp;\n{{end}}\n<p>\n\n{{range $i, $f := .Files}}\n\n<div class=thumbnail>\n   <a href=\"{{$f.Url}}\"><img src=\"/image/?path={{$.LocalPath}}/{{$f.Name}}\"></img></a>\n</div>\n\n{{end}}\n\n</body>\n" +
+	"<html>\n<head>\n   <title>Pictures in {{$.Path}}</title>\n   <link href=\"/s/style.css\" media=\"all\" rel=\"stylesheet\" />\n</head>\n<body>\n<div class=page>\n\n{{range $i, $b := .Breadcrum}}\n<a href=\"{{$b.Url}}\">{{$b.Name}}</a> \n{{end}}\n\n<h2>{{.Title}} - {{$.LocalPath}}</h2>\n\n{{if ..Directories}}\n<div class=directories>\n   {{range $i, $f := .Directories}}\n   <a href=\"{{$f.Url}}\">{{$f.Basename}}</a> &nbsp;\n   {{end}}\n   <p>\n</div>\n{{end}}\n\n<div class=container>\n   {{range $i, $f := .Files}}\n   <div class=thumbnail>\n      <a href=\"{{$f.Url}}\"><img src=\"/image/?path={{$.LocalPath}}/{{$f.Name}}\"></img></a>\n   </div>\n   {{end}}\n</div>\n\n\n</div>\n</body>\n</html>\n" +
 	""}))
