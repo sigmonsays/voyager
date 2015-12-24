bc431b2ca182650478734ec4b37341ff3cff284e
Sig Lange <sig.lange@gmail.com>
2015-12-24 12:08:09 -0800

    use go-bindata

--- asset/picture.html.gen.go
+++
@@ -1,7 +0,0 @@
-// AUTOMATICALLY GENERATED FILE. DO NOT EDIT.
-
-package asset
-
-var picture = Blob(asset.init(asset{Name: "picture.html", Content: "" +
-	"<html>\n<head>\n   <title>Pictures in {{$.Path}}</title>\n   <link href=\"/s/style.css\" media=\"all\" rel=\"stylesheet\" />\n</head>\n<body>\n<div class=page>\n\n{{range $i, $b := .Breadcrum}}\n<a href=\"{{$b.Url}}\">{{$b.Name}}</a> \n{{end}}\n\n<h2>Pictures in {{.Path}}</h2>\n\n{{if ..Directories}}\n<div class=directories>\n   {{range $i, $f := .Directories}}\n   <a href=\"{{$f.Url}}\">{{$f.Basename}}</a> &nbsp;\n   {{end}}\n   <p>\n</div>\n{{end}}\n\n<div class=container>\n   {{range $i, $f := .Files}}\n   <div class=thumbnail>\n      <a href=\"{{$f.Url}}\"><img src=\"/image/?path={{$.LocalPath}}/{{$f.Name}}\"></img></a>\n   </div>\n   {{end}}\n</div>\n\n\n</div>\n</body>\n</html>\n" +
-	""}))
--- asset/style.css.gen.go
+++
@@ -1,7 +0,0 @@
-// AUTOMATICALLY GENERATED FILE. DO NOT EDIT.
-
-package asset
-
-var style = Blob(asset.init(asset{Name: "style.css", Content: "" +
-	"body {\n}\n\na {\n   color: #0066FF;\n}\n\n.thumbnail {\n   margin: 5px;\n   padding: 2px;\n   float: left;\n   display: block;\n   width: 128px;\n   height: 128px;\n   overflow: hidden;\n}\n\nh1, h2, h3, h4 {\n   padding: 0px;\n   margin: 0px;\n}\n\ndiv.page {\n}\n\ndiv.directories {\n   padding: 5px;\n   border: 1px solid grey;\n   background-color: #EBEBEB;\n}\ndiv.container {\n   background-color: #EBEBEB;\n}\n" +
-	""}))
