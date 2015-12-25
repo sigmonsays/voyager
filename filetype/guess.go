9d63c095d8ea618b37e1c66cfc0164085e00d3a4
Sig Lange <sig.lange@gmail.com>
2015-12-25 12:39:53 -0800

    Fix guess layout

--- filetype/guess.go
+++ filetype/guess.go
@@ -5,23 +5,24 @@ import (
 )
 
 func GuessLayout(localpath string, filenames []string) FileType {
-	found := make(map[FileType]int)
+	totals := make(map[FileType]int)
 	for _, filename := range filenames {
 		ftype, err := Determine(filepath.Join(localpath, filename))
 		if err != nil {
 			log.Warnf("determine filetype %s: %s", filename, err)
 		}
-		if _, ok := found[ftype]; ok == false {
-			found[ftype] = 0
+		if _, ok := totals[ftype]; ok == false {
+			totals[ftype] = 0
 		}
-		found[ftype]++
+		totals[ftype]++
 	}
+	log.Tracef("localpath:%s totals:%+v", localpath, totals)
 	var layout FileType
 	var numfiles int
-	for ftype, cnt := range found {
+	for ftype, cnt := range totals {
 		if ftype != UnknownFile && cnt > numfiles {
 			layout = ftype
-			cnt = numfiles
+			numfiles = cnt
 		}
 	}
 	log.Debugf("guessed layout for %s as %s", localpath, layout)
