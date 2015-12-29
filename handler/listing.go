0f11ebf51b4473b1e81fd27b53a0226c49140b00
Sig Lange <sig.lange@gmail.com>
2015-12-29 13:55:16 -0800

    changes

--- handler/listing.go
+++ handler/listing.go
@@ -16,11 +16,10 @@ func NewListHandler(handler *Handler) *ListHandler {
 }
 func (h *ListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	strip_prefix := h.Handler.UrlPrefix
-	log.Debugf("serve path:%s (stripPrefix:%s urlPrefix:%s localPath:%s rootPath:%s)",
-		r.URL.Path, strip_prefix, h.UrlPrefix, h.LocalPath, h.RootPath)
+	log.Debugf("serve path:%s (stripPrefix:%s relpath:%s urlPrefix:%s localPath:%s rootPath:%s)",
+		r.URL.Path, strip_prefix, h.RelPath, h.UrlPrefix, h.LocalPath, h.RootPath)
 
-	// handler := http.StripPrefix(strip_prefix, http.FileServer(http.Dir(h.LocalPath)))
+	handler := http.StripPrefix(h.RelPath, http.FileServer(http.Dir(h.LocalPath)))
 
-	handler := http.FileServer(http.Dir(h.LocalPath))
 	handler.ServeHTTP(w, r)
 }
